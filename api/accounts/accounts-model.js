fd81c2545d58dd575fcfb5f16d61250eb7dcf872
Markeisha <markeisha94@gmail.com>
2022-02-14 20:14:03 -0600

    added accounts router

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,32 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('acounts');
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  return db('accounts').where('id', id).first();
 }
 
 const create = account => {
   // DO YOUR MAGIC
+  const [ id ] = db('accounts').insert(account);
+  return getById(id);
 }
 
-const updateById = (id, account) => {
+const updateById = async({id, account}) => {
   // DO YOUR MAGIC
+  await db('accounts').where('id', id).update({id, account});
+  return getById(id);
 }
 
-const deleteById = id => {
+const deleteById = async (id) => {
   // DO YOUR MAGIC
+  const deletedPost = await getById(id);
+  await db('accounts').where('id', id).delete();
+  return deletedPost;
 }
 
 module.exports = {
